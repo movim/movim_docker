975c1fdc2a5eaf83d6cb3ca0d6876aff99d89875
Kane Valentine <kane@cute.im>
2017-06-29 19:08:54 -0400

    upload base files

---
+++ movim/config/db.inc.php
@@ -0,0 +1,8 @@
+$conf = [
+    'type'        => 'pgsql',
+    'username'    => '{$POSTGRES_USER}',
+    'password'    => '{$POSTGRES_PASSWORD}',
+    'host'        => 'postgres',
+    'port'        => 5432,
+    'database'    => '{$POSTGRES_DB}'
+];
---
+++ movim/scripts/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp /opt/config/db.inc.php /var/www/config/db.inc.php
+
+service php7.0-fpm start
+
+chown -R www-data: /var/www/
+chmod -R u+rwx /var/www/
+
+su - www-data php /var/www/daemon.php start --url=http://{$MOVIM_DOMAIN}/ --port={$MOVIM_PORT} --interface={$MOVIM_INTERFACE} --verbose --debug "$@"
