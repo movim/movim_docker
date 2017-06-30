723ce953bcc999d16fc8a77ea0dee52394ad72d8
Kane Valentine <kane@cute.im>
2017-06-30 15:06:47 +0100

    edit to nginx config and fix to database variables

--- movim/config/db.inc.php
+++ movim/config/db.inc.php
@@ -1,8 +1,8 @@
 $conf = [
     'type'        => 'pgsql',
-    'username'    => '{$POSTGRES_USER}',
-    'password'    => '{$POSTGRES_PASSWORD}',
+    'username'    => '$POSTGRES_USER',
+    'password'    => '$POSTGRES_PASSWORD',
     'host'        => 'postgres',
     'port'        => 5432,
-    'database'    => '{$POSTGRES_DB}'
+    'database'    => '$POSTGRES_DB'
 ];
