096396a1cc12396bf001a89708211969f9e88fe4
Kane Valentine <kane@cute.im>
2017-06-30 10:58:10 +0100

    change to init.sh, chmod/chown command

--- movim/scripts/init.sh
+++ movim/scripts/init.sh
@@ -8,7 +8,7 @@ git pull # To update the Movim source-code
 php composer.phar install # To update the libraries
 php mud.php db --set # Adapt Movim schema if required
 
-chown -R www-data: /var/www/
-chmod -R u+rwx /var/www/
+chown -R www-data:www-data /var/www/ \
+&& chmod -R u+rwx /var/www/
 
 su - www-data php /var/www/daemon.php start --url={$MOVIM_DOMAIN} --port={$MOVIM_PORT} --interface={$MOVIM_INTERFACE} --verbose --debug "$@"
