8c6c88e7869b008ace922e58d9610809855f17dc
Kane Valentine <kane@cute.im>
2017-06-30 09:40:39 +0100

    added Movim update into init

--- movim/scripts/init.sh
+++ movim/scripts/init.sh
@@ -4,6 +4,10 @@ cp /opt/config/db.inc.php /var/www/movim/config/db.inc.php
 
 service php7.0-fpm start
 
+git pull # To update the Movim source-code
+php composer.phar install # To update the libraries
+php mud.php db --set # Adapt Movim schema if required
+
 chown -R www-data: /var/www/
 chmod -R u+rwx /var/www/
 
