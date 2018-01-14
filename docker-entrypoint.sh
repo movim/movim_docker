68250fa9ba57b2dee66f321984cbf7011ed2cb46
Kane Valentine <kane@cute.im>
2018-01-14 15:48:57 +0000

    added basic entrypoint to copy movim files and set database config

---
+++ docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if ! [ -e index.php -a -e daemon.php ]; then
+	echo >&2 "Movim not found in $PWD - copying now..."
+	if [ "$(ls -A)" ]; then
+		echo >&2 "WARNING: $PWD is not empty - press Ctrl+C now if this is an error!"
+		( set -x; ls -A; sleep 10 )
+	fi
+	tar cf - --one-file-system -C /usr/src/movim-${MOVIM_VERSION} . | tar xf -
+	echo >&2 "Complete! Movim ${MOVIM_VERSION} has been successfully copied to $PWD"
+fi
+
+cat <<EOT > config/db.inc.php
+<?php
+\$conf = [
+    'type'        => 'pgsql',
+    'database'    => '$POSTGRES_DB',
+    'host'        => '$POSTGRES_HOST',
+    'port'        => '$POSTGRES_PORT',
+    'username'    => '$POSTGRES_USER',
+    'password'    => '$POSTGRES_PASSWORD',
+];
+EOT
+
+exec "$@"
