e2697085cdbc1f7c1076b28bec0a15105b5dfcf7
Kane Valentine <kane@cute.im>
2020-05-17 03:29:06 +0100

    fix #24

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! [ -e index.php -a -e daemon.php ]; then
+if ! [ -e daemon.php -a -e public/index.php ]; then
 	echo >&2 "Movim not found in $PWD - copying now..."
 	if [ "$(ls -A)" ]; then
 		echo >&2 "WARNING: $PWD is not empty - press Ctrl+C now if this is an error!"
